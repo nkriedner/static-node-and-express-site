4813269e97a72acbac07f9f2f1432d95365fc97a
Nils Kriedner <nkriedner@googlemail.com>
2021-07-29 12:53:20 +0200

    Create basic code for server in app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+    console.log("Get request to / route.");
+    res.send("<h1>Home!!!</h1>");
+});
+
+app.listen(3000, () => console.log("App is running on port 3000..."));

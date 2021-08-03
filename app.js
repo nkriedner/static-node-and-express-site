af78db71c8a96bdad2e6fd1210d1b6fa87443923
Nils Kriedner <nkriedner@googlemail.com>
2021-08-03 16:43:27 +0200

    Change some css styles + add process.env to app.listen

--- app.js
+++ app.js
@@ -68,4 +68,6 @@ app.use((err, req, res, next) => {
 // ******************
 // MAKE SERVER LISTEN
 // ******************
-app.listen(3000, () => console.log("App is running on port 3000."));
+app.listen(process.env.PORT || 3000, () =>
+    console.log("App is running on port 3000.")
+);
